b0200b6ae1e38586c2f79241fb85973e6762ef7c
David Delassus <david.jose.delassus@gmail.com>
2024-08-23 12:38:46 +0200

    :white_check_mark: add benchmark test suite

---
+++ tests/benchmark/generate-logs.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+from argparse import ArgumentParser
+import tomllib
+
+from datetime import datetime
+from time import sleep
+import random
+
+from urllib.request import Request, urlopen
+import json
+
+
+def format_syslog(hostname: str, appname: str, message: str) -> str:
+    timestamp = datetime.now().strftime("%b %d %H:%M:%S")
+    return f"{timestamp} {hostname} {appname}: {message}"
+
+
+def send_log(log: str):
+    payload = json.dumps({"record": {"message": log}}).encode("utf-8")
+    req = Request(
+        "http://localhost:5080/api/v1/pipelines/test/logs",
+        data=payload,
+        headers={"Content-Type": "application/json"},
+    )
+
+    with urlopen(req) as resp:
+        assert resp.status == 200
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("--conftest", default="conftest.toml")
+    parser.add_argument("--req-per-sec", type=int, default=100)
+    parser.add_argument("--req-count", type=int, default=1_000_000)
+    args = parser.parse_args()
+
+    with open(args.conftest, "rb") as f:
+        data = tomllib.load(f)
+
+    req_count = args.req_count
+    while req_count > 0:
+        perc = (args.req_count - req_count) / args.req_count * 100
+        print(f"Progress: {perc}%")
+
+        batch_count = 0
+        while batch_count < args.req_per_sec:
+            hostname = random.choice(data["hosts"])
+            app = random.choice(data["apps"])
+            message = random.choice(app["messages"])
+
+            send_log(format_syslog(hostname, app["name"], message))
+
+            batch_count += 1
+
+        req_count = max(0, req_count - batch_count)
+        sleep(1)
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/benchmark/run.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+../../bin/flowg \
+    -db ./data/logs \
+    -config ./data/config \
+    -bind 127.0.0.1:5080 \
+    -verbose \
+  > logs.txt &
+pid=$!
+trap "kill $pid" EXIT
+
+sleep 0.1
+
+python generate-logs.py
