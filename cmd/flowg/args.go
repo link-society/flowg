1affe573a6ca0b7fb3e53463477d455bf9c960b7
David Delassus <david.jose.delassus@gmail.com>
2024-08-24 10:59:48 +0200

    :fire: remove CLI flags set by dependencies

--- cmd/flowg/args.go
+++ cmd/flowg/args.go
@@ -1,14 +1,43 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"os"
+)
 
 var (
-	bindAddress = flag.String("bind", ":5080", "Address to bind the server to")
-	logDir      = flag.String("log-dir", "./data/logs", "Path to the log database directory")
-	configDir   = flag.String("config-dir", "./data/config", "Path to the config directory")
-	verbose     = flag.Bool("verbose", false, "Enable verbose logging")
+	bindAddress *string
+	logDir      *string
+	configDir   *string
+	verbose     *bool
 )
 
 func init() {
+	flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+
+	bindAddress = flag.String(
+		"bind",
+		":5080",
+		"Address to bind the server to",
+	)
+
+	logDir = flag.String(
+		"log-dir",
+		"./data/logs",
+		"Path to the log database directory",
+	)
+
+	configDir = flag.String(
+		"config-dir",
+		"./data/config",
+		"Path to the config directory",
+	)
+
+	verbose = flag.Bool(
+		"verbose",
+		false,
+		"Enable verbose logging",
+	)
+
 	flag.Parse()
 }
