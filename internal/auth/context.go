6a616732d13c805434fc8aaf22778b6d042128f9
David Delassus <david.jose.delassus@gmail.com>
2024-08-25 18:26:02 +0200

    :passport_control: add `Bearer` authentication to API using Personal Access Tokens

---
+++ internal/auth/context.go
@@ -0,0 +1,42 @@
+package auth
+
+import (
+	"context"
+	"time"
+)
+
+type authContext struct {
+	parent   context.Context
+	username *string
+}
+
+type authContextKey string
+
+const CONTEXT_USERNAME authContextKey = "auth_context_username"
+
+func ContextWithUsername(ctx context.Context, username string) context.Context {
+	return &authContext{
+		parent:   ctx,
+		username: &username,
+	}
+}
+
+func (ac *authContext) Deadline() (deadline time.Time, ok bool) {
+	return ac.parent.Deadline()
+}
+
+func (ac *authContext) Done() <-chan struct{} {
+	return ac.parent.Done()
+}
+
+func (ac *authContext) Err() error {
+	return ac.parent.Err()
+}
+
+func (ac *authContext) Value(key interface{}) interface{} {
+	if key == CONTEXT_USERNAME {
+		return ac.username
+	}
+
+	return ac.parent.Value(key)
+}
