58a6c21b5805c722d1638d2b5e0f079022b66020
David Delassus <david.jose.delassus@gmail.com>
2024-08-28 17:11:07 +0200

    :lock: allow only the current user to create personal access tokens for himself

--- api/create_token.go
+++ api/create_token.go
@@ -11,9 +11,7 @@ import (
 	"link-society.com/flowg/internal/auth"
 )
 
-type CreateTokenRequest struct {
-	User string `path:"user" minLength:"1"`
-}
+type CreateTokenRequest struct{}
 
 type CreateTokenResponse struct {
 	Success bool   `json:"success"`
@@ -22,79 +20,76 @@ type CreateTokenResponse struct {
 
 func CreateTokenUsecase(authDb *auth.Database) usecase.Interactor {
 	u := usecase.NewInteractor(
-		auth.RequireScopeApiMiddleware(
-			authDb,
-			auth.SCOPE_WRITE_ACLS,
-			func(
-				ctx context.Context,
-				req CreateTokenRequest,
-				resp *CreateTokenResponse,
-			) error {
-				user, err := authDb.GetUser(req.User)
-				if err != nil {
-					slog.ErrorContext(
-						ctx,
-						"Failed to get user",
-						"channel", "api",
-						"user", req.User,
-						"error", err.Error(),
-					)
+		func(
+			ctx context.Context,
+			req CreateTokenRequest,
+			resp *CreateTokenResponse,
+		) error {
+			username := auth.GetContextUser(ctx)
+			user, err := authDb.GetUser(username)
+			if err != nil {
+				slog.ErrorContext(
+					ctx,
+					"Failed to get user",
+					"channel", "api",
+					"user", username,
+					"error", err.Error(),
+				)
 
-					resp.Success = false
-					return status.Wrap(err, status.Internal)
-				}
+				resp.Success = false
+				return status.Wrap(err, status.Internal)
+			}
 
-				if user == nil {
-					slog.ErrorContext(
-						ctx,
-						"User not found",
-						"channel", "api",
-						"user", req.User,
-					)
+			if user == nil {
+				slog.ErrorContext(
+					ctx,
+					"User not found",
+					"channel", "api",
+					"user", username,
+				)
 
-					resp.Success = false
-					return status.Wrap(errors.New("user not found"), status.NotFound)
-				}
+				resp.Success = false
+				return status.Wrap(errors.New("user not found"), status.NotFound)
+			}
 
-				token, err := auth.NewToken(32)
-				if err != nil {
-					slog.ErrorContext(
-						ctx,
-						"Failed to create token",
-						"channel", "api",
-						"user", req.User,
-						"error", err.Error(),
-					)
+			token, err := auth.NewToken(32)
+			if err != nil {
+				slog.ErrorContext(
+					ctx,
+					"Failed to create token",
+					"channel", "api",
+					"user", user.Name,
+					"error", err.Error(),
+				)
 
-					resp.Success = false
-					return status.Wrap(err, status.Internal)
-				}
+				resp.Success = false
+				return status.Wrap(err, status.Internal)
+			}
 
-				err = authDb.AddPersonalAccessToken(user.Name, token)
-				if err != nil {
-					slog.ErrorContext(
-						ctx,
-						"Failed to save token",
-						"channel", "api",
-						"user", req.User,
-						"error", err.Error(),
-					)
+			err = authDb.AddPersonalAccessToken(user.Name, token)
+			if err != nil {
+				slog.ErrorContext(
+					ctx,
+					"Failed to save token",
+					"channel", "api",
+					"user", user.Name,
+					"error", err.Error(),
+				)
 
-					resp.Success = false
-					return status.Wrap(err, status.Internal)
-				}
+				resp.Success = false
+				return status.Wrap(err, status.Internal)
+			}
 
-				resp.Success = true
-				resp.Token = token
+			resp.Success = true
+			resp.Token = token
 
-				return nil
-			},
-		),
+			return nil
+		},
 	)
 
 	u.SetName("create_token")
 	u.SetTitle("Create Token")
-	u.SetDescription("Create a new Personal Access Token for a user")
+	u.SetDescription("Create a new Personal Access Token for the current user")
 	u.SetTags("acls")
 
 	u.SetExpectedErrors(status.PermissionDenied, status.NotFound, status.Internal)
