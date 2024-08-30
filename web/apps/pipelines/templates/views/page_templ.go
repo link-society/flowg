3e49afe996037878cef145e6f1d1e4bb07624237
David Delassus <david.jose.delassus@gmail.com>
2024-08-31 00:22:59 +0200

    :fire: remove floating action buttons in pipelines editor

--- web/apps/pipelines/templates/views/page_templ.go
+++ web/apps/pipelines/templates/views/page_templ.go
@@ -85,12 +85,6 @@ func Page(props PageProps) templ.Component {
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
-			if props.Permissions.CanEditPipelines {
-				templ_7745c5c3_Err = components.FABs().Render(ctx, templ_7745c5c3_Buffer)
-				if templ_7745c5c3_Err != nil {
-					return templ_7745c5c3_Err
-				}
-			}
 			return templ_7745c5c3_Err
 		})
 		templ_7745c5c3_Err = layouts.App(layouts.AppProps{
