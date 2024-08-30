6ed67ab3b0026a01ad94b467f15e9d04842ff9c3
David Delassus <david.jose.delassus@gmail.com>
2024-08-30 18:57:39 +0200

    :wrench: :sparkles: generate default role, user and pipeline if none exists during startup

---
+++ internal/app/bootstrap/auth.go
@@ -0,0 +1,54 @@
+package bootstrap
+
+import (
+	"fmt"
+
+	"link-society.com/flowg/internal/data/auth"
+)
+
+func DefaultRolesAndUsers(authDb *auth.Database) error {
+	roleSys := auth.NewRoleSystem(authDb)
+	userSys := auth.NewUserSystem(authDb)
+
+	roles, err := roleSys.ListRoles()
+	if err != nil {
+		return err
+	}
+
+	if len(roles) == 0 {
+		adminRole := auth.Role{
+			Name: "admin",
+			Scopes: []auth.Scope{
+				auth.SCOPE_SEND_LOGS,
+				auth.SCOPE_WRITE_ACLS,
+				auth.SCOPE_WRITE_PIPELINES,
+				auth.SCOPE_WRITE_TRANSFORMERS,
+				auth.SCOPE_WRITE_STREAMS,
+			},
+		}
+
+		err := roleSys.SaveRole(adminRole)
+		if err != nil {
+			return fmt.Errorf("failed to bootstrap admin role: %w", err)
+		}
+	}
+
+	users, err := userSys.ListUsers()
+	if err != nil {
+		return err
+	}
+
+	if len(users) == 0 {
+		rootUser := auth.User{
+			Name:  "root",
+			Roles: []string{"admin"},
+		}
+
+		err := userSys.SaveUser(rootUser, "root")
+		if err != nil {
+			return fmt.Errorf("failed to bootstrap root user: %w", err)
+		}
+	}
+
+	return nil
+}
