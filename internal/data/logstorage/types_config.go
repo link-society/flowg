4004812c0c8b46a0f38c7948c4a502ce96d7f519
David Delassus <david.jose.delassus@gmail.com>
2024-09-01 23:31:42 +0200

    :card_file_box: add indexed fields to stream config

--- internal/data/logstorage/types_config.go
+++ internal/data/logstorage/types_config.go
@@ -1,6 +1,7 @@
 package logstorage
 
 type StreamConfig struct {
-	RetentionTime int64 `json:"ttl" description:"TTL in seconds, 0 to disable"`
-	RetentionSize int64 `json:"size" description:"Maximum size in MB, 0 to disable"`
+	RetentionTime int64    `json:"ttl" description:"TTL in seconds, 0 to disable"`
+	RetentionSize int64    `json:"size" description:"Maximum size in MB, 0 to disable"`
+	IndexedFields []string `json:"indexed_fields"`
 }
