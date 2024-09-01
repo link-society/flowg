78b5d97b788f26be31339234c910d9408b10383a
David Delassus <david.jose.delassus@gmail.com>
2024-09-01 11:18:50 +0200

    :bug: always initialize notifications array

--- internal/webutils/notify.go
+++ internal/webutils/notify.go
@@ -13,7 +13,9 @@ type notifyContextKey string
 const CONTEXT_NOTIFY_SYSTEM notifyContextKey = "notify_system"
 
 func WithNotificationSystem(ctx context.Context) context.Context {
-	return context.WithValue(ctx, CONTEXT_NOTIFY_SYSTEM, &notifySystem{})
+	return context.WithValue(ctx, CONTEXT_NOTIFY_SYSTEM, &notifySystem{
+		notifications: make([]string, 0),
+	})
 }
 
 func NotifyInfo(ctx context.Context, message string) {
