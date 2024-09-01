207eb1ca6e9ec17171fab6faf8bd77457e7a0a56
David Delassus <david.jose.delassus@gmail.com>
2024-09-01 03:01:18 +0200

    :recycle: use retention size in MB

--- internal/data/logstorage/types_config.go
+++ internal/data/logstorage/types_config.go
@@ -1,6 +1,6 @@
 package logstorage
 
 type StreamConfig struct {
-	RetentionTime int64 `json:"ttl"`
-	RetentionSize int64 `json:"size"`
+	RetentionTime int64 `json:"ttl" description:"TTL in seconds"`
+	RetentionSize int64 `json:"size" description:"Maximum size in MB"`
 }
