7ae41a93c32ed99fdce2db6be8d2f48016e0d681
David Delassus <david.jose.delassus@gmail.com>
2024-09-01 03:21:18 +0200

    :lipstick: remove `validate` class from input fields in storage view

--- web/apps/storage/templates/components/retention_tab_templ.go
+++ web/apps/storage/templates/components/retention_tab_templ.go
@@ -53,33 +53,33 @@ func RetentionTab(props RetentionTabProps) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\"><section class=\"input-field\"><input id=\"retention_time\" type=\"number\" class=\"validate\" name=\"retention_time\" min=\"0\" value=\"")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\"><section class=\"input-field\"><input id=\"retention_time\" type=\"number\" name=\"retention_time\" min=\"0\" value=\"")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
 		var templ_7745c5c3_Var3 string
 		templ_7745c5c3_Var3, templ_7745c5c3_Err = templ.JoinStringErrs(fmt.Sprintf("%d", props.Config.RetentionTime))
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `apps/storage/templates/components/retention_tab.templ`, Line: 27, Col: 62}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `apps/storage/templates/components/retention_tab.templ`, Line: 26, Col: 62}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var3))
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\"> <label for=\"retention_days\">Retention Time (in seconds)</label></section><section class=\"input-field\"><input id=\"retention_size\" type=\"number\" class=\"validate\" name=\"retention_size\" min=\"0\" value=\"")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\"> <label for=\"retention_days\">Retention Time (in seconds) - 0 to disable</label></section><section class=\"input-field\"><input id=\"retention_size\" type=\"number\" name=\"retention_size\" min=\"0\" value=\"")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
 		var templ_7745c5c3_Var4 string
 		templ_7745c5c3_Var4, templ_7745c5c3_Err = templ.JoinStringErrs(fmt.Sprintf("%d", props.Config.RetentionSize))
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `apps/storage/templates/components/retention_tab.templ`, Line: 39, Col: 62}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `apps/storage/templates/components/retention_tab.templ`, Line: 37, Col: 62}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var4))
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\"> <label for=\"retention_size\">Retention Size (MB)</label></section><button class=\"btn waves-effect waves-light\" type=\"submit\"")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\"> <label for=\"retention_size\">Retention Size (MB) - 0 to disable</label></section><button class=\"btn waves-effect waves-light\" type=\"submit\"")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
