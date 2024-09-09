cd87998f120b862c22be456d09671d251942d6b8
David Delassus <david.jose.delassus@gmail.com>
2024-09-09 16:41:17 +0200

    :card_file_box: add `config.AlertSystem` to `config.Storage`

---
+++ internal/data/config/system_alerts.go
@@ -0,0 +1,73 @@
+package config
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"path/filepath"
+
+	"link-society.com/flowg/internal/data/alerting"
+)
+
+const ALERTS_STORAGE_TYPE = "alerts"
+
+type AlertSystem struct {
+	backend *Storage
+}
+
+func NewAlertSystem(backend *Storage) *AlertSystem {
+	backend.resolveStorageTypeDir(ALERTS_STORAGE_TYPE)
+	return &AlertSystem{backend: backend}
+}
+
+func (sys *AlertSystem) List() ([]string, error) {
+	items, err := sys.backend.listStorageTypeItems(ALERTS_STORAGE_TYPE)
+	if err != nil {
+		return nil, err
+	}
+
+	results := []string{}
+
+	for _, item := range items {
+		if filepath.Ext(item) == ".json.b64" {
+			results = append(results, item[:len(item)-9])
+		}
+	}
+
+	return results, nil
+}
+
+func (sys *AlertSystem) Read(name string) (*alerting.Webhook, error) {
+	b64content, err := sys.backend.readStorageTypeItem(ALERTS_STORAGE_TYPE, name+".json.b64")
+	if err != nil {
+		return nil, err
+	}
+
+	content, err := base64.StdEncoding.DecodeString(b64content)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decode webhook %s: %w", name, err)
+	}
+
+	webhook := &alerting.Webhook{}
+	err = json.Unmarshal(content, webhook)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal webhook %s: %w", name, err)
+	}
+
+	return webhook, nil
+}
+
+func (sys *AlertSystem) Write(name string, webhook *alerting.Webhook) error {
+	content, err := json.Marshal(webhook)
+	if err != nil {
+		return fmt.Errorf("failed to marshal webhook %s: %w", name, err)
+	}
+
+	b64content := base64.StdEncoding.EncodeToString(content)
+
+	return sys.backend.writeStorageTypeItem(ALERTS_STORAGE_TYPE, name+".json.b64", b64content)
+}
+
+func (sys *AlertSystem) Delete(name string) error {
+	return sys.backend.deleteStorageTypeItem(ALERTS_STORAGE_TYPE, name+".json.b64")
+}
