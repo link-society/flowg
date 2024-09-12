afeebaa20afee26a1b26cadd2c4a4364af34c4c9
David Delassus <david.jose.delassus@gmail.com>
2024-09-12 20:34:19 +0200

    :sparkles: add `whoami` API operation

--- api/main.go
+++ api/main.go
@@ -94,6 +94,8 @@ func NewHandler(
 		r.Put("/api/v1/users/{user}", SaveUserUsecase(authDb))
 		r.Delete("/api/v1/users/{user}", DeleteUserUsecase(authDb))
 
+		r.Get("/api/v1/whoami", WhoamiUsecase(authDb))
+
 		r.Get("/api/v1/tokens", ListTokensUsecase(authDb))
 		r.Post("/api/v1/token", CreateTokenUsecase(authDb))
 		r.Delete("/api/v1/tokens/{token-uuid}", DeleteTokenUsecase(authDb))
