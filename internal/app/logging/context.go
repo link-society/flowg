e03e82410ebf767f731a56b756c4392cceb2fe2f
David Delassus <david.jose.delassus@gmail.com>
2024-09-12 19:54:36 +0200

    :fire: remove useless re-implementation of `context.WithValue`

--- internal/app/logging/context.go
+++ internal/app/logging/context.go
@@ -2,7 +2,6 @@ package logging
 
 import (
 	"context"
-	"time"
 
 	"github.com/google/uuid"
 )
@@ -10,30 +9,6 @@ import (
 type key string
 
 const CORRELATION_ID = key("correlationId")
-
-type correlatedContext struct {
-	parent        context.Context
-	correlationId string
-}
-
-func (c *correlatedContext) Deadline() (deadline time.Time, ok bool) {
-	return c.parent.Deadline()
-}
-
-func (c *correlatedContext) Done() <-chan struct{} {
-	return c.parent.Done()
-}
-
-func (c *correlatedContext) Err() error {
-	return c.parent.Err()
-}
-
-func (c *correlatedContext) Value(key any) any {
-	if key == CORRELATION_ID {
-		return c.correlationId
-	}
-	return c.parent.Value(key)
-}
 
 func WithCorrelationId(
 	ctx context.Context,
@@ -43,8 +18,5 @@ func WithCorrelationId(
 		correlationId = uuid.New().String()
 	}
 
-	return &correlatedContext{
-		parent:        ctx,
-		correlationId: correlationId,
-	}
+	return context.WithValue(ctx, CORRELATION_ID, correlationId)
 }
