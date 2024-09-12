433072a442e3df41cddd5769d204c94e9e20a9dd
David Delassus <david.jose.delassus@gmail.com>
2024-09-12 19:00:50 +0200

    :white_check_mark: add `bootstrap` test suite

---
+++ internal/app/bootstrap/pipelines_test.go
@@ -0,0 +1,33 @@
+package bootstrap_test
+
+import (
+	"testing"
+
+	"link-society.com/flowg/internal/data/config"
+
+	"link-society.com/flowg/internal/app/bootstrap"
+)
+
+func TestDefaultPipeline(t *testing.T) {
+	opts := config.DefaultStorageOpts().WithInMemory(true)
+	configStorage := config.NewStorage(opts)
+
+	err := bootstrap.DefaultPipeline(configStorage)
+	if err != nil {
+		t.Fatalf("failed to create default pipeline: %v", err)
+	}
+
+	pipelineSys := config.NewPipelineSystem(configStorage)
+	pipelines, err := pipelineSys.List()
+	if err != nil {
+		t.Fatalf("failed to list pipelines: %v", err)
+	}
+
+	if len(pipelines) != 1 {
+		t.Fatalf("expected 1 pipeline, got %d", len(pipelines))
+	}
+
+	if pipelines[0] != "default" {
+		t.Fatalf("expected pipeline name to be default, got %s", pipelines[0])
+	}
+}
