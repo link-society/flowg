36048c5e811a4dc7d8af75612f911159aba3afa1
David Delassus <david.jose.delassus@gmail.com>
2024-09-12 13:22:49 +0200

    :sparkles: add "In Memory" mode for logstorage

--- internal/data/logstorage/main.go
+++ internal/data/logstorage/main.go
@@ -9,19 +9,42 @@ import (
 	"link-society.com/flowg/internal/app/logging"
 )
 
+type StorageOpts struct {
+	dir      string
+	inMemory bool
+}
+
+func DefaultStorageOpts() StorageOpts {
+	return StorageOpts{
+		dir:      "./data/logs",
+		inMemory: false,
+	}
+}
+
+func (s StorageOpts) WithDir(dir string) StorageOpts {
+	s.dir = dir
+	return s
+}
+
+func (s StorageOpts) WithInMemory(inMemory bool) StorageOpts {
+	s.inMemory = inMemory
+	return s
+}
+
 type Storage struct {
 	db      *badger.DB
 	gc      *garbageCollector
 	indexer *indexer
 }
 
-func NewStorage(dbPath string) (*Storage, error) {
-	opts := badger.
-		DefaultOptions(dbPath).
+func NewStorage(opts StorageOpts) (*Storage, error) {
+	dbOpts := badger.
+		DefaultOptions(opts.dir).
 		WithLogger(&logging.BadgerLogger{Channel: "logstorage"}).
-		WithCompression(options.ZSTD)
+		WithCompression(options.ZSTD).
+		WithInMemory(opts.inMemory)
 
-	db, err := badger.Open(opts)
+	db, err := badger.Open(dbOpts)
 	if err != nil {
 		return nil, err
 	}
