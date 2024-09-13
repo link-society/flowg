4a458ca731c37ca8a208cceed10c4322dad8bd50
David Delassus <david.jose.delassus@gmail.com>
2024-09-13 02:00:29 +0200

    :white_check_mark: add stress test and html/junit report of integration tests

--- tests/e2e/run.sh
+++ tests/e2e/run.sh
@@ -7,4 +7,12 @@ set -e
 hurl \
   --variable admin_token=${FLOWG_ADMIN_TOKEN} \
   --variable guest_token=${FLOWG_GUEST_TOKEN} \
-  --test specs/
+  --report-html reports/html \
+  --report-junit reports/junit.xml \
+  --test integration/
+
+hurl \
+  --variable admin_token=${FLOWG_ADMIN_TOKEN} \
+  --variable guest_token=${FLOWG_GUEST_TOKEN} \
+  --repeat 1000 \
+  --test benchmark/
