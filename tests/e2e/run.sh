b58552d2e75addf75ab411b064c62f7c854fafd2
David Delassus <david.jose.delassus@gmail.com>
2024-09-13 13:36:06 +0200

    :white_check_mark: add test suite for ACL API

--- tests/e2e/run.sh
+++ tests/e2e/run.sh
@@ -9,6 +9,7 @@ hurl \
   --variable guest_token=${FLOWG_GUEST_TOKEN} \
   --variable timewindow_begin=$(date -d "5 minutes ago" -u +"%Y-%m-%dT%H:%M:%SZ") \
   --variable timewindow_end=$(date -d "+5 minutes" -u +"%Y-%m-%dT%H:%M:%SZ") \
+  --error-format long \
   --report-html reports/html \
   --report-junit reports/junit.xml \
   --jobs 1 \
