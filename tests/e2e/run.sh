36235b80c004bbd14a79f8ba45c4038749b81843
David Delassus <david.jose.delassus@gmail.com>
2024-09-13 04:12:14 +0200

    :white_check_mark: add retention test suite

--- tests/e2e/run.sh
+++ tests/e2e/run.sh
@@ -7,12 +7,18 @@ set -e
 hurl \
   --variable admin_token=${FLOWG_ADMIN_TOKEN} \
   --variable guest_token=${FLOWG_GUEST_TOKEN} \
+  --variable timewindow_begin=$(date -d "5 minutes ago" -u +"%Y-%m-%dT%H:%M:%SZ") \
+  --variable timewindow_end=$(date -d "+5 minutes" -u +"%Y-%m-%dT%H:%M:%SZ") \
   --report-html reports/html \
   --report-junit reports/junit.xml \
+  --jobs 1 \
   --test integration/
 
-hurl \
-  --variable admin_token=${FLOWG_ADMIN_TOKEN} \
-  --variable guest_token=${FLOWG_GUEST_TOKEN} \
-  --repeat 1000 \
-  --test benchmark/
+if [ -z "$NOBENCHMARK" ]
+then
+  hurl \
+    --variable admin_token=${FLOWG_ADMIN_TOKEN} \
+    --variable guest_token=${FLOWG_GUEST_TOKEN} \
+    --repeat 1000 \
+    --test benchmark/
+fi
