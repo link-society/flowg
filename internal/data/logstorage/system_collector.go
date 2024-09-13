9eee7a5bf4d0137faf24bf6d6548d1fb727a21e3
David Delassus <david.jose.delassus@gmail.com>
2024-09-13 02:00:57 +0200

    :bug: retry transaction on conflict when ingesting logs

--- internal/data/logstorage/system_collector.go
+++ internal/data/logstorage/system_collector.go
@@ -24,87 +24,101 @@ func (sys *CollectorSystem) Ingest(
 	stream string,
 	logEntry *LogEntry,
 ) ([]byte, error) {
-	key := logEntry.NewDbKey(stream)
-	val, err := json.Marshal(logEntry)
-	if err != nil {
-		slog.DebugContext(
-			ctx,
-			"Could not marshal log entry",
-			"channel", "storage",
-			"stream", stream,
-			"error", err.Error(),
-		)
-		return nil, fmt.Errorf("could not marshal log entry: %w", err)
-	}
-
-	err = sys.storage.db.Update(func(txn *badger.Txn) error {
-		slog.DebugContext(
-			ctx,
-			"Fetch stream configuration",
-			"channel", "storage",
-			"stream", stream,
-		)
-
-		streamConfig, err := getOrCreateStreamConfig(txn, stream)
+	for {
+		key := logEntry.NewDbKey(stream)
+		val, err := json.Marshal(logEntry)
 		if err != nil {
-			return err
-		}
-
-		slog.DebugContext(
-			ctx,
-			"Save log entry in BadgerDB",
-			"channel", "storage",
-			"stream", stream,
-			"key", key,
-		)
-
-		entry := badger.NewEntry(key, val)
-		if streamConfig.RetentionTime > 0 {
-			entry = entry.WithTTL(time.Duration(streamConfig.RetentionTime) * time.Second)
-		}
-
-		if err := txn.SetEntry(entry); err != nil {
-			return fmt.Errorf(
-				"could not add log entry '%s' to stream '%s': %w",
-				key, stream, err,
+			slog.DebugContext(
+				ctx,
+				"Could not marshal log entry",
+				"channel", "storage",
+				"stream", stream,
+				"error", err.Error(),
 			)
+			return nil, fmt.Errorf("could not marshal log entry: %w", err)
 		}
 
-		for field, value := range logEntry.Fields {
-			fieldKey := []byte(fmt.Sprintf("stream:field:%s:%s", stream, field))
-			if err := txn.Set(fieldKey, []byte{}); err != nil {
+		err = sys.storage.db.Update(func(txn *badger.Txn) error {
+			slog.DebugContext(
+				ctx,
+				"Fetch stream configuration",
+				"channel", "storage",
+				"stream", stream,
+			)
+
+			streamConfig, err := getOrCreateStreamConfig(txn, stream)
+			if err != nil {
+				return err
+			}
+
+			slog.DebugContext(
+				ctx,
+				"Save log entry in BadgerDB",
+				"channel", "storage",
+				"stream", stream,
+				"key", key,
+			)
+
+			entry := badger.NewEntry(key, val)
+			if streamConfig.RetentionTime > 0 {
+				entry = entry.WithTTL(time.Duration(streamConfig.RetentionTime) * time.Second)
+			}
+
+			if err := txn.SetEntry(entry); err != nil {
 				return fmt.Errorf(
-					"could not save field '%s' of log entry '%s' to stream '%s': %w",
-					field, key, stream, err,
+					"could not add log entry '%s' to stream '%s': %w",
+					key, stream, err,
 				)
 			}
 
-			if streamConfig.IsFieldIndexed(field) {
-				slog.DebugContext(
-					ctx,
-					"Save field index in BadgerDB",
-					"channel", "storage",
-					"stream", stream,
-					"key", key,
-					"field", field,
-				)
-
-				fieldIndex := newFieldIndex(txn, stream, field, value)
-				if err := fieldIndex.AddKey(key, streamConfig.RetentionTime); err != nil {
+			for field, value := range logEntry.Fields {
+				fieldKey := []byte(fmt.Sprintf("stream:field:%s:%s", stream, field))
+				if err := txn.Set(fieldKey, []byte{}); err != nil {
 					return fmt.Errorf(
-						"could not add field index '%s' of log entry '%s' to stream '%s': %w",
+						"could not save field '%s' of log entry '%s' to stream '%s': %w",
 						field, key, stream, err,
 					)
 				}
+
+				if streamConfig.IsFieldIndexed(field) {
+					slog.DebugContext(
+						ctx,
+						"Save field index in BadgerDB",
+						"channel", "storage",
+						"stream", stream,
+						"key", key,
+						"field", field,
+					)
+
+					fieldIndex := newFieldIndex(txn, stream, field, value)
+					if err := fieldIndex.AddKey(key, streamConfig.RetentionTime); err != nil {
+						return fmt.Errorf(
+							"could not add field index '%s' of log entry '%s' to stream '%s': %w",
+							field, key, stream, err,
+						)
+					}
+				}
 			}
+
+			return nil
+		})
+
+		switch err {
+		case nil:
+			return key, nil
+
+		case badger.ErrConflict:
+			slog.DebugContext(
+				ctx,
+				"Retry log entry ingestion",
+				"channel", "storage",
+				"stream", stream,
+				"key", key,
+			)
+			continue
+
+		default:
+			return nil, err
 		}
-
-		return nil
-	})
-
-	if err != nil {
-		return nil, err
 	}
-
-	return key, nil
 }
