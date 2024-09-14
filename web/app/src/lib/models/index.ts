ec768f4b6848b2f7b1b257455309ed5d9a8ce1cb
David Delassus <david.jose.delassus@gmail.com>
2024-09-14 18:27:49 +0200

    :construction: add skeleton react frontend

---
+++ web/app/src/lib/models/index.ts
@@ -0,0 +1,4 @@
+export type UserModel = {
+  name: string
+  roles: string[]
+}
---
+++ web/app/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: './',
+  plugins: [
+    react(),
+  ],
+  resolve: {
+    alias: {
+      '@materializecss/materialize/style': 'node_modules/@materializecss/materialize/dist/css/materialize.css',
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    sourcemap: true,
+  },
+})
