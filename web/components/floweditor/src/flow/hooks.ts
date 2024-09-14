557451fba5f1d1e114aa6edbaf34db082e574be1
David Delassus <david.jose.delassus@gmail.com>
2024-09-14 18:35:13 +0200

    :fire: remove obsolete code

--- web/components/floweditor/src/flow/hooks.ts
+++
@@ -1,8 +0,0 @@
-import React, { createContext } from 'react'
-import { Node } from '@xyflow/react'
-
-export default createContext<{
-  setNodes: React.Dispatch<React.SetStateAction<Node[]>>
-}>({
-  setNodes: () => {},
-})
