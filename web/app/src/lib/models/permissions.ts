2fe97ea6ac016fe3a6a906dd7f916d895369881a
David Delassus <david.jose.delassus@gmail.com>
2024-09-17 02:36:52 +0200

    :card_file_box: add permissions model

---
+++ web/app/src/lib/models/permissions.ts
@@ -0,0 +1,27 @@
+export const SCOPES = [
+  'read_pipelines',
+  'write_pipelines',
+  'read_transformers',
+  'write_transformers',
+  'read_streams',
+  'write_streams',
+  'read_alerts',
+  'write_alerts',
+  'read_acls',
+  'write_acls',
+  'send_logs',
+]
+
+export const SCOPE_LABELS = {
+  read_pipelines: 'View Pipelines',
+  write_pipelines: 'View & Edit Pipelines',
+  read_transformers: 'View Transformers',
+  write_transformers: 'View & Edit Transformers',
+  read_streams: 'View Streams',
+  write_streams: 'View & Edit Streams',
+  read_alerts: 'View Alerts',
+  write_alerts: 'View & Edit Alerts',
+  read_acls: 'View ACLs',
+  write_acls: 'View & Edit ACLs',
+  send_logs: 'Send Logs',
+}
