32f7671a724b27f78cba1088e03cab03e95057d5
David Delassus <david.jose.delassus@gmail.com>
2024-09-18 10:36:04 +0200

    :sparkles: add all transformer API operations to client

---
+++ web/app/src/lib/api/operations/tests.tsx
@@ -0,0 +1,25 @@
+import * as request from '@/lib/api/request'
+
+export const testTransformer = async (
+  code: string,
+  record: Record<string, string>,
+): Promise<Record<string, string>> => {
+  type TestTransformerRequest = {
+    code: string
+    record: Record<string, string>
+  }
+
+  type TestTransformerResponse = {
+    success: boolean
+    record: Record<string, string>
+  }
+
+  const { body } = await request.POST<TestTransformerRequest, TestTransformerResponse>(
+    '/api/v1/test/transformer',
+    {
+      code,
+      record,
+    },
+  )
+  return body.record
+}
