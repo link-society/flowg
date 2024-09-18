afc528542c4b47ef5c9eb9f3f27aff19badc5d36
David Delassus <david.jose.delassus@gmail.com>
2024-09-18 12:01:55 +0200

    :lipstick: add create button to transformer view when no data

--- web/app/src/views/app/transformers/section.tsx
+++ web/app/src/views/app/transformers/section.tsx
@@ -4,6 +4,8 @@ import { useLoaderData, useNavigate } from 'react-router-dom'
 import Backdrop from '@mui/material/Backdrop'
 import CircularProgress from '@mui/material/CircularProgress'
 
+import { NewTransformerButton } from './new-btn'
+
 export const TransformerView = () => {
   const { transformers } = useLoaderData() as { transformers: string[] }
   const navigate = useNavigate()
@@ -26,7 +28,11 @@ export const TransformerView = () => {
           </Backdrop>
         )
         : (
-          <div>No data</div>
+          <div className="w-full h-full flex flex-col items-center justify-center gap-5">
+            <h1 className="text-3xl font-semibold">No transformer found, create one</h1>
+
+            <NewTransformerButton />
+          </div>
         )
       }
     </>
