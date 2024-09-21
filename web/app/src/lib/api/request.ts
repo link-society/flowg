da9c8765b7733c133c6d404a6b990e3408c5332a
David Delassus <david.jose.delassus@gmail.com>
2024-09-21 03:08:20 +0200

    :sparkles: add API client primitive for Server Sent Events

--- web/app/src/lib/api/request.ts
+++ web/app/src/lib/api/request.ts
@@ -1,3 +1,5 @@
+import { EventSourcePlus } from 'event-source-plus'
+
 import * as errors from '@/lib/api/errors'
 
 type ApiMethod = 'GET' | 'POST' | 'PUT' | 'DELETE'
@@ -89,4 +91,66 @@ export const DELETE = async<R extends { success: boolean }>(
   options: RequestOptions<never>,
 ): Promise<ApiRequestResult<R>> => {
   return request('DELETE', options)
+}
+
+export const SSE = (
+  options: RequestOptions<never>,
+) => {
+  const eventSource = new EventSourcePlus(
+    `${options.path}?${options.searchParams?.toString() ?? ''}`,
+    {
+      maxRetryCount: 0,
+      headers: {
+        'Authorization': `Bearer jwt:${localStorage.getItem('token')}`,
+      },
+    },
+  )
+
+  const messages = new EventTarget()
+  const control = new EventTarget()
+
+  const cancelScope = eventSource.listen({
+    onRequestError({ error }) {
+      cancelScope.abort()
+      control.dispatchEvent(new CustomEvent('error', { detail: error }))
+    },
+    async onResponseError({ response }) {
+      try {
+        const responseBody = await response.json()
+        const content = responseBody as ApiErrorResponse
+
+        switch (content.status) {
+          case 'UNAUTHENTICATED':
+            throw new errors.UnauthenticatedError(content.error)
+
+          case 'PERMISSION_DENIED':
+            throw new errors.PermissionDeniedError(content.error)
+
+          default:
+            throw new errors.ApiError(content.error)
+        }
+      }
+      catch (error) {
+        cancelScope.abort()
+        control.dispatchEvent(new CustomEvent('error', { detail: error }))
+      }
+    },
+    onMessage(msg) {
+      const detail = {
+        id: msg.id,
+        data: msg.data,
+        retry: msg.retry,
+      }
+
+      messages.dispatchEvent(new CustomEvent(msg.event, { detail }))
+    }
+  })
+
+  return {
+    messages,
+    control,
+    close() {
+      cancelScope.abort()
+    }
+  }
 }
