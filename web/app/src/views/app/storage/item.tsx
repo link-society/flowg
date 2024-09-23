84a65dbe55e0f7f469e2684496330810eb2d4880
David Delassus <david.jose.delassus@gmail.com>
2024-09-23 11:22:41 +0200

    :recycle: move stream editor out of stream view

--- web/app/src/views/app/storage/item.tsx
+++ web/app/src/views/app/storage/item.tsx
@@ -7,19 +7,17 @@ import { useNotify } from '@/lib/hooks/notify'
 import HelpIcon from '@mui/icons-material/Help'
 import DeleteIcon from '@mui/icons-material/Delete'
 import SaveIcon from '@mui/icons-material/Save'
-import AddIcon from '@mui/icons-material/Add'
 
 import Box from '@mui/material/Box'
 import Grid from '@mui/material/Grid2'
-import Divider from '@mui/material/Divider'
 import Paper from '@mui/material/Paper'
 import List from '@mui/material/List'
 import ListItemButton from '@mui/material/ListItemButton'
 import ListItemText from '@mui/material/ListItemText'
 import Button from '@mui/material/Button'
 import CircularProgress from '@mui/material/CircularProgress'
-import TextField from '@mui/material/TextField'
 
+import { StreamEditor } from '@/components/editors/stream'
 import { NewStreamButton } from './new-btn'
 
 import * as configApi from '@/lib/api/operations/config'
@@ -35,7 +33,6 @@ export const StreamView = () => {
   const streamNames = Object.keys(streams)
 
   const [streamConfig, setStreamConfig] = useState(streams[currentStream!]!)
-  const [newField, setNewField] = useState('')
 
   const onCreate = useCallback(
     (name: string) => {
@@ -148,119 +145,10 @@ export const StreamView = () => {
           </Paper>
         </Grid>
         <Grid size={{ xs: 10 }} className="h-full">
-          <div className="h-full flex flex-row items-stretch gap-3">
-            <Paper className="h-full flex-1 flex flex-col items-stretch">
-              <h1 className="p-3 bg-gray-100 text-xl text-center font-semibold">Retention</h1>
-              <Divider />
-
-              <div
-                className="
-                  p-3 flex-grow flex-shrink h-0 overflow-auto
-                  flex flex-col items-stretch gap-3
-                "
-              >
-                <TextField
-                  label="Retention size (in MB)"
-                  variant="outlined"
-                  type="number"
-                  value={streamConfig.size}
-                  onChange={(e) => {
-                    setStreamConfig((prev) => ({
-                      ...prev,
-                      size: Number(e.target.value),
-                    }))
-                  }}
-                />
-
-                <TextField
-                  label="Retention time (in seconds)"
-                  variant="outlined"
-                  type="number"
-                  value={streamConfig.ttl}
-                  onChange={(e) => {
-                    setStreamConfig((prev) => ({
-                      ...prev,
-                      ttl: Number(e.target.value),
-                    }))
-                  }}
-                />
-
-                <p className="italic">Use <code className="font-mono bg-gray-200 text-red-500 px-1">0</code> to disable</p>
-              </div>
-            </Paper>
-
-            <Paper className="h-full flex-1 flex flex-col items-stretch">
-              <h1 className="p-3 bg-gray-100 text-xl text-center font-semibold">Indexes</h1>
-              <Divider />
-
-              <div
-                className="
-                  p-3 flex-grow flex-shrink h-0 overflow-auto
-                  flex flex-col items-stretch gap-3
-                "
-              >
-                {streamConfig.indexed_fields.map((field) => (
-                  <div key={field} className="flex flex-row items-stretch gap-3">
-                    <TextField
-                      label="Field"
-                      variant="outlined"
-                      type="text"
-                      value={field}
-                      onChange={(e) => {
-                        setStreamConfig((prev) => ({
-                          ...prev,
-                          indexed_fields: prev.indexed_fields.map((f) => f === field ? e.target.value : f),
-                        }))
-                      }}
-                      className="flex-grow"
-                    />
-
-                    <Button
-                      variant="contained"
-                      color="error"
-                      size="small"
-                      onClick={() => {
-                        setStreamConfig((prev) => ({
-                          ...prev,
-                          indexed_fields: prev.indexed_fields.filter((f) => f !== field),
-                        }))
-                      }}
-                    >
-                      <DeleteIcon />
-                    </Button>
-                  </div>
-                ))}
-
-                <div className="flex flex-row items-stretch gap-3">
-                  <TextField
-                    label="Field"
-                    variant="outlined"
-                    type="text"
-                    value={newField}
-                    onChange={(e) => {
-                      setNewField(e.target.value)
-                    }}
-                    className="flex-grow"
-                  />
-
-                  <Button
-                    variant="contained"
-                    color="primary"
-                    size="small"
-                    onClick={() => {
-                      setStreamConfig((prev) => ({
-                        ...prev,
-                        indexed_fields: [...prev.indexed_fields, newField],
-                      }))
-                      setNewField('')
-                    }}
-                  >
-                    <AddIcon />
-                  </Button>
-                </div>
-              </div>
-            </Paper>
-          </div>
+          <StreamEditor
+            streamConfig={streamConfig}
+            onStreamConfigChange={setStreamConfig}
+          />
         </Grid>
       </Grid>
     </Box>
