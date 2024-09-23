37fa77c1d195f2c81877c3064cb3aa27964ea541
David Delassus <david.jose.delassus@gmail.com>
2024-09-23 11:22:41 +0200

    :recycle: move stream editor out of stream view

---
+++ web/app/src/components/editors/stream/index.tsx
@@ -0,0 +1,140 @@
+import { useState } from 'react'
+
+import AddIcon from '@mui/icons-material/Add'
+import DeleteIcon from '@mui/icons-material/Delete'
+
+import Divider from '@mui/material/Divider'
+import Paper from '@mui/material/Paper'
+import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField'
+
+import { StreamConfigModel } from '@/lib/models'
+
+type StreamEditorProps = {
+  streamConfig: StreamConfigModel
+  onStreamConfigChange: (config: StreamConfigModel) => void
+}
+
+export const StreamEditor = ({ streamConfig, onStreamConfigChange }: StreamEditorProps) => {
+  const [newField, setNewField] = useState('')
+
+  return (
+    <div className="h-full flex flex-row items-stretch gap-3">
+      <Paper className="h-full flex-1 flex flex-col items-stretch">
+        <h1 className="p-3 bg-gray-100 text-xl text-center font-semibold">Retention</h1>
+        <Divider />
+
+        <div
+          className="
+            p-3 flex-grow flex-shrink h-0 overflow-auto
+            flex flex-col items-stretch gap-3
+          "
+        >
+          <TextField
+            label="Retention size (in MB)"
+            variant="outlined"
+            type="number"
+            value={streamConfig.size}
+            onChange={(e) => {
+              onStreamConfigChange({
+                ...streamConfig,
+                size: Number(e.target.value),
+              })
+            }}
+          />
+
+          <TextField
+            label="Retention time (in seconds)"
+            variant="outlined"
+            type="number"
+            value={streamConfig.ttl}
+            onChange={(e) => {
+              onStreamConfigChange({
+                ...streamConfig,
+                ttl: Number(e.target.value),
+              })
+            }}
+          />
+
+          <p className="italic">Use <code className="font-mono bg-gray-200 text-red-500 px-1">0</code> to disable</p>
+        </div>
+      </Paper>
+
+      <Paper className="h-full flex-1 flex flex-col items-stretch">
+        <h1 className="p-3 bg-gray-100 text-xl text-center font-semibold">Indexes</h1>
+        <Divider />
+
+        <div
+          className="
+            p-3 flex-grow flex-shrink h-0 overflow-auto
+            flex flex-col items-stretch gap-3
+          "
+        >
+          {streamConfig.indexed_fields.map((field) => (
+            <div key={field} className="flex flex-row items-stretch gap-3">
+              <TextField
+                label="Field"
+                variant="outlined"
+                type="text"
+                value={field}
+                onChange={(e) => {
+                  onStreamConfigChange({
+                    ...streamConfig,
+                    indexed_fields: streamConfig.indexed_fields.map(
+                      (f) => f === field ? e.target.value : f
+                    ),
+                  })
+                }}
+                className="flex-grow"
+              />
+
+              <Button
+                variant="contained"
+                color="error"
+                size="small"
+                onClick={() => {
+                  onStreamConfigChange({
+                    ...streamConfig,
+                    indexed_fields: streamConfig.indexed_fields.filter(
+                      (f) => f !== field
+                    ),
+                  })
+                }}
+              >
+                <DeleteIcon />
+              </Button>
+            </div>
+          ))}
+
+          <div className="flex flex-row items-stretch gap-3">
+            <TextField
+              label="Field"
+              variant="outlined"
+              type="text"
+              value={newField}
+              onChange={(e) => {
+                setNewField(e.target.value)
+              }}
+              className="flex-grow"
+            />
+
+            <Button
+              variant="contained"
+              color="primary"
+              size="small"
+              onClick={() => {
+                onStreamConfigChange({
+                  ...streamConfig,
+                  indexed_fields: [...streamConfig.indexed_fields, newField],
+                })
+                setNewField('')
+              }}
+            >
+              <AddIcon />
+            </Button>
+          </div>
+        </div>
+      </Paper>
+    </div>
+  )
+}
