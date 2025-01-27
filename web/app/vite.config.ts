a15c73c37bfd33660cb0196cdfab33bd943b5183
David Delassus <david.jose.delassus@gmail.com>
2025-01-27 14:44:41 +0100

    :wrench: update vite configuration with new tailwindcss plugin

--- web/app/vite.config.ts
+++ web/app/vite.config.ts
@@ -4,25 +4,19 @@ import fs from 'fs'
 import { defineConfig } from 'vite'
 import { compression } from 'vite-plugin-compression2'
 import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
   plugins: [
     react(),
+    tailwindcss(),
     compression({
       filename: '[path][base]',
       deleteOriginalAssets: true
     }),
   ],
-  css: {
-    postcss: {
-      plugins: [
-        tailwindcss,
-      ],
-    },
-  },
   build: {
     chunkSizeWarningLimit: 1024,
     rollupOptions: {
