675bfbdfd819515f6d5343c0e32bcea5a8df2e37
lav <lav@saashup.com>
2025-02-11 09:41:07 +0100

    :tada: fix version and missing piece of code

--- web/app/src/main.tsx
+++ web/app/src/main.tsx
@@ -13,3 +13,31 @@ import {
 
 ModuleRegistry.registerModules([AllCommunityModule]);
 provideGlobalGridOptions({ theme: "legacy"});
+
+
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import './styles/main.css'
+import '@xyflow/react/dist/style.css'
+import 'ag-grid-community/styles/ag-grid.css'
+import 'ag-grid-community/styles/ag-theme-material.css'
+import 'ag-grid-community/styles/ag-theme-balham.css'
+import './styles/table.css'
+
+import App from '@/App'
+
+import {
+  AllCommunityModule,
+  ModuleRegistry,
+  provideGlobalGridOptions
+} from 'ag-grid-community'
+
+ModuleRegistry.registerModules([AllCommunityModule]);
+provideGlobalGridOptions({ theme: "legacy"});
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
