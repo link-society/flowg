387bd4b06961528f598d6abf754c30dadb7fa591
David Delassus <david.jose.delassus@gmail.com>
2025-03-03 12:10:54 +0100

    :whale: update binary name in docker image

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -6,18 +6,4 @@ mkdir -p /data
 chmod 0700 /data
 chown -R flowg:flowg /data
 
-case "$1" in
-  serve)
-    shift
-    exec su-exec flowg /usr/local/bin/flowg serve $@
-    ;;
-
-  admin)
-    shift
-    exec su-exec flowg /usr/local/bin/flowg admin $@
-    ;;
-
-  *)
-    exec su-exec flowg /usr/local/bin/flowg --help
-    ;;
-esac
+exec su-exec flowg /usr/local/bin/flowg-server $@
