974380410ef8c188a5108402024a3f75619d26b5
David Delassus <david.jose.delassus@gmail.com>
2025-03-05 17:46:22 +0100

    :bug: resolve to private IP address if listener is listening on all interfaces

--- internal/services/mgmt/main.go
+++ internal/services/mgmt/main.go
@@ -1,12 +1,17 @@
 package mgmt
 
 import (
+	"fmt"
 	"log/slog"
+	"net"
 
 	"crypto/tls"
 	"net/url"
 
+	"github.com/hashicorp/go-sockaddr"
+
 	"link-society.com/flowg/internal/cluster"
+
 	"link-society.com/flowg/internal/utils/proctree"
 )
 
@@ -39,17 +44,34 @@ func NewServer(opts *ServerOptions) proctree.Process {
 		JoinNodeEndpoint: opts.ClusterJoinEndpoint,
 		Cookie:           opts.ClusterCookie,
 
-		LocalEndpointResolver: func() *url.URL {
+		LocalEndpointResolver: func() (*url.URL, error) {
+			host, port, err := net.SplitHostPort(listenerH.listener.Addr().String())
+			if err != nil {
+				return nil, fmt.Errorf("failed to parse listener address: %w", err)
+			}
+
+			if host == "0.0.0.0" || host == "::" {
+				ip, err := sockaddr.GetPrivateIP()
+				if err != nil {
+					return nil, fmt.Errorf("failed to get private IP: %w", err)
+				}
+				if ip == "" {
+					return nil, fmt.Errorf("no private IP found")
+				}
+
+				host = ip
+			}
+
 			localEndpoint := &url.URL{
 				Scheme: "http",
-				Host:   listenerH.listener.Addr().String(),
+				Host:   net.JoinHostPort(host, port),
 			}
 
 			if opts.TlsConfig != nil {
 				localEndpoint.Scheme = "https"
 			}
 
-			return localEndpoint
+			return localEndpoint, nil
 		},
 	})
 
