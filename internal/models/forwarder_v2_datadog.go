221b6436ce34cc8796a4ef7995fbcd4cd4539f05
Harshit Gupta <hg2t4e@gmail.com>
2025-03-29 13:10:45 +0100

    :sparkles: Changes based on the discussion in the PR, thereby finalising the datadog forwarder

--- internal/models/forwarder_v2_datadog.go
+++ internal/models/forwarder_v2_datadog.go
@@ -6,29 +6,16 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"strings"
 )
 
 type ForwarderDatadogV2 struct {
-	Type    string `json:"type" enum:"datadog"`
-	Url     string `json:"url"`
-	ApiKey  string `json:"apiKey"`
-	Source  string `json:"source"`
-	Service string `json:"service"`
-}
-
-type DatadogLogItem struct {
-	DdSource string `json:"ddsource"`
-	DdTags   string `json:"ddtags"`
-	Hostname string `json:"hostname"`
-	Message  string `json:"message"`
-	Service  string `json:"service"`
+	Type   string `json:"type" enum:"datadog"`
+	Url    string `json:"url"`
+	ApiKey string `json:"apiKey"`
 }
 
 func (f *ForwarderDatadogV2) call(ctx context.Context, record *LogRecord) error {
-	logItems := []*DatadogLogItem{CreateDatadogHttpLogItem(f, record)}
-
-	payload, err := json.Marshal(logItems)
+	payload, err := json.Marshal(record)
 	if err != nil {
 		return fmt.Errorf("failed to marshal log record: %w", err)
 	}
@@ -39,7 +26,6 @@ func (f *ForwarderDatadogV2) call(ctx context.Context, record *LogRecord) error 
 		return fmt.Errorf("failed to create request: %w", err)
 	}
 
-	//TODO is this hardcoded the way to go?
 	req.Header.Add("Accept", "application/json")
 	req.Header.Add("Content-Type", "application/json")
 	req.Header.Add("DD-API-KEY", f.ApiKey)
@@ -55,26 +41,4 @@ func (f *ForwarderDatadogV2) call(ctx context.Context, record *LogRecord) error 
 	}
 
 	return nil
-}
-
-func CreateDatadogHttpLogItem(f *ForwarderDatadogV2, record *LogRecord) *DatadogLogItem {
-	logItem := &DatadogLogItem{}
-	var tags []string
-
-	for key, value := range record.Fields {
-		switch key {
-		case f.Service:
-			logItem.Service = value
-		case f.Source:
-			logItem.DdSource = value
-		case "message": /* TODO we should not hard code this */
-			logItem.Message = value
-		case "hostname": /* TODO we should not hard code this */
-			logItem.Hostname = value
-		default:
-			tags = append(tags, fmt.Sprintf("%s:%s", key, value))
-		}
-	}
-	logItem.DdTags = strings.Join(tags, ",")
-	return logItem
 }
--- web/app/src/lib/models/forwarder/datadog.ts
+++ web/app/src/lib/models/forwarder/datadog.ts
@@ -2,6 +2,4 @@ export type DatadogForwarderModel = {
     type: 'datadog'
     url: string
     apiKey: string
-    service: string
-    source: string
   }
