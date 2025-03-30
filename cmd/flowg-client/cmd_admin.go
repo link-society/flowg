0485b838bac534a345d7da4ec9b1ca833d9fbf10
David Delassus <david.jose.delassus@gmail.com>
2025-03-30 17:10:45 +0200

    :sparkles: add `admin backup` and `admin restore` commands

---
+++ cmd/flowg-client/cmd_admin.go
@@ -0,0 +1,17 @@
+package main
+
+import "github.com/spf13/cobra"
+
+func NewAdminCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "admin",
+		Short: "Admin commands",
+	}
+
+	cmd.AddCommand(
+		NewAdminBackupCommand(),
+		NewAdminRestoreCommand(),
+	)
+
+	return cmd
+}
