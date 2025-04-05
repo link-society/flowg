25f288aa8bde933277ad4fcc8c106df04ce2aee0
David Delassus <david.jose.delassus@gmail.com>
2025-04-05 15:36:15 +0200

    :wrench: update boostrapped pipeline

--- internal/app/bootstrap/pipelines.go
+++ internal/app/bootstrap/pipelines.go
@@ -18,51 +18,54 @@ func DefaultPipeline(ctx context.Context, configStorage *config.Storage) error {
 			ctx,
 			"default",
 			`{
+				"version": 2,
 				"nodes": [
 					{
 						"id": "__builtin__source_direct",
 						"type": "source",
-						"position": {"x": 210, "y": 195},
-						"deletable": false,
-						"data": {"type": "direct"},
-						"measured": {"width": 136, "height": 38},
-						"selected": true,
-						"dragging": false
+						"position": {
+							"x": -45,
+							"y": 0
+						},
+						"data": {
+							"type": "direct"
+						}
 					},
 					{
 						"id": "__builtin__source_syslog",
 						"type": "source",
-						"position": {"x": 210, "y": 250},
-						"deletable": false,
-						"data": {"type": "syslog"},
-						"measured": {"width": 136, "height": 38},
-						"selected": true,
-						"dragging": false
+						"position": {
+							"x": -45,
+							"y": 120
+						},
+						"data": {
+							"type": "syslog"
+						}
 					},
 					{
-						"id": "node-1",
+						"id": "node-37da64b3-6243-4620-b0f2-ba484a71b053",
 						"type": "router",
-						"position": {"x": 405, "y": 195},
-						"data": {"stream": "default"},
-						"measured": {"width": 241,"height": 91},
-						"selected": false,
-						"dragging": false
+						"position": {
+							"x": 345,
+							"y": 60
+						},
+						"data": {
+							"stream": "default"
+						}
 					}
 				],
 				"edges": [
 					{
-						"id": "xy-edge____builtin__source_direct-node-1",
-						"type": "smoothstep",
-						"source": "__builtin__source_direct",
-						"target": "node-1",
-						"animated": true
+						"id": "xy-edge____builtin__source_syslog-node-37da64b3-6243-4620-b0f2-ba484a71b053",
+						"source": "__builtin__source_syslog",
+						"sourceHandle": "",
+						"target": "node-37da64b3-6243-4620-b0f2-ba484a71b053"
 					},
 					{
-						"id": "xy-edge____builtin__source_syslog-node-1",
-						"type": "smoothstep",
-						"source": "__builtin__source_syslog",
-						"target": "node-1",
-						"animated": true
+						"id": "xy-edge____builtin__source_direct-node-37da64b3-6243-4620-b0f2-ba484a71b053",
+						"source": "__builtin__source_direct",
+						"sourceHandle": "",
+						"target": "node-37da64b3-6243-4620-b0f2-ba484a71b053"
 					}
 				]
 			}`,
