402a8080211c02fd1684863c8139b02fad4fb3ee
David Delassus <david.jose.delassus@gmail.com>
2025-04-19 22:19:54 +0200

    :bug: parse properly OTLP `AnyValue` type

--- internal/models/logrecord.go
+++ internal/models/logrecord.go
@@ -2,10 +2,13 @@ package models
 
 import (
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/google/uuid"
-	otlpmodels "go.opentelemetry.io/proto/otlp/logs/v1"
+
+	otlpcommonmodels "go.opentelemetry.io/proto/otlp/common/v1"
+	otlplogmodels "go.opentelemetry.io/proto/otlp/logs/v1"
 )
 
 type LogRecord struct {
@@ -20,11 +23,11 @@ func NewLogRecord(fields map[string]string) *LogRecord {
 	}
 }
 
-func NewFromOTLP(logRecord *otlpmodels.LogRecord) *LogRecord {
+func NewFromOTLP(logRecord *otlplogmodels.LogRecord) *LogRecord {
 	fields := map[string]string{
 		"severity_number":          logRecord.SeverityNumber.String(),
 		"severity_text":            logRecord.SeverityText,
-		"body":                     logRecord.Body.String(),
+		"body":                     otlpValueParser(logRecord.Body),
 		"dropped_attributes_count": fmt.Sprintf("%d", logRecord.DroppedAttributesCount),
 		"flags":                    fmt.Sprintf("%d", logRecord.Flags),
 		"trace_id":                 fmt.Sprintf("%x", logRecord.TraceId),
@@ -35,8 +38,7 @@ func NewFromOTLP(logRecord *otlpmodels.LogRecord) *LogRecord {
 	}
 	for _, attribute := range logRecord.Attributes {
 		fieldName := fmt.Sprintf("attr.%s", attribute.Key)
-		fieldValue := attribute.Value.String()
-		fields[fieldName] = fieldValue
+		fields[fieldName] = otlpValueParser(attribute.Value)
 	}
 
 	return &LogRecord{
@@ -52,4 +54,40 @@ func (e *LogRecord) NewDbKey(stream string) []byte {
 		e.Timestamp.UnixMilli(),
 		uuid.New().String(),
 	))
+}
+
+func otlpValueParser(v *otlpcommonmodels.AnyValue) string {
+	switch v.Value.(type) {
+	case *otlpcommonmodels.AnyValue_StringValue:
+		return v.GetStringValue()
+	case *otlpcommonmodels.AnyValue_BoolValue:
+		return fmt.Sprintf("%t", v.GetBoolValue())
+	case *otlpcommonmodels.AnyValue_IntValue:
+		return fmt.Sprintf("%d", v.GetIntValue())
+	case *otlpcommonmodels.AnyValue_DoubleValue:
+		return fmt.Sprintf("%f", v.GetDoubleValue())
+	case *otlpcommonmodels.AnyValue_ArrayValue:
+		items := make([]string, 0, len(v.GetArrayValue().Values))
+
+		for i, item := range v.GetArrayValue().Values {
+			items[i] = otlpValueParser(item)
+		}
+
+		return fmt.Sprintf("[%s]", strings.Join(items, " "))
+
+	case *otlpcommonmodels.AnyValue_KvlistValue:
+		items := make([]string, 0, len(v.GetKvlistValue().Values))
+
+		for i, item := range v.GetKvlistValue().Values {
+			items[i] = fmt.Sprintf("%s:%s", item.Key, otlpValueParser(item.Value))
+		}
+
+		return fmt.Sprintf("map[%s]", strings.Join(items, " "))
+
+	case *otlpcommonmodels.AnyValue_BytesValue:
+		return fmt.Sprintf("%v", v.GetBytesValue())
+
+	default:
+		return v.String()
+	}
 }
