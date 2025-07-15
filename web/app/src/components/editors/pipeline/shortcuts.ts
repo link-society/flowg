e211de47193ea7865b9bb3f73512cab78c698def
David Delassus <david.jose.delassus@gmail.com>
2025-07-15 01:28:06 +0000

    :lipstick: add support for `Delete` key to remove nodes or edges in the pipeline editor

---
+++ web/app/src/components/editors/pipeline/shortcuts.ts
@@ -0,0 +1,7 @@
+import { KeyCode } from '@xyflow/react'
+
+export const deleteKeyCode: KeyCode = ['Backspace', 'Delete']
+export const selectionKeyCode: KeyCode = 'Shift'
+export const panActivationKeyCode: KeyCode = 'Space'
+export const multiSelectionKeyCode: KeyCode = ['Meta', 'Control']
+export const zoomActivationKeyCode: KeyCode = ['Meta', 'Control']
