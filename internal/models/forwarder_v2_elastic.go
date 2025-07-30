e4f840666a06e0efc83f1c9e80e62c9f46b861d1
David Delassus <david.jose.delassus@gmail.com>
2025-07-30 16:15:45 +0000

    :card_file_box: add elasticsearch forwarder datamodel

---
+++ internal/models/forwarder_v2_elastic.go
@@ -0,0 +1,81 @@
+package models
+
+import (
+	"context"
+	"fmt"
+
+	"bytes"
+	"encoding/base64"
+	"encoding/json"
+
+	"github.com/elastic/go-elasticsearch/v9"
+)
+
+type ForwarderElasticV2 struct {
+	Type      string   `json:"type" enum:"elastic"`
+	Index     string   `json:"index"`
+	Addresses []string `json:"addresses"`
+	CACert    string   `json:"ca,omitempty"`
+	Token     string   `json:"token,omitempty"`
+}
+
+func (f *ForwarderElasticV2) call(ctx context.Context, record *LogRecord) error {
+	var caBytes []byte
+	if f.CACert != "" {
+		var err error
+		caBytes, err = base64.StdEncoding.DecodeString(f.CACert)
+		if err != nil {
+			return fmt.Errorf("failed to decode CA certificate: %w", err)
+		}
+	}
+
+	cfg := elasticsearch.Config{
+		Addresses:    f.Addresses,
+		CACert:       caBytes,
+		ServiceToken: f.Token,
+	}
+	client, err := elasticsearch.NewClient(cfg)
+	if err != nil {
+		return fmt.Errorf("failed to create ElasticSearch client: %w", err)
+	}
+
+	resp, err := client.Indices.Exists([]string{f.Index}, client.Indices.Exists.WithContext(ctx))
+	if err != nil {
+		return fmt.Errorf("failed to send ElasticSearch request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.IsError() {
+		if resp.StatusCode == 404 {
+			resp, err := client.Indices.Create(f.Index, client.Indices.Create.WithContext(ctx))
+			if err != nil {
+				return fmt.Errorf("failed to send ElasticSearch request: %w", err)
+			}
+			defer resp.Body.Close()
+
+			if resp.IsError() {
+				return fmt.Errorf("failed to check index '%s': %s", f.Index, resp.String())
+			}
+		} else {
+			return fmt.Errorf("failed to check index '%s': %s", f.Index, resp.String())
+		}
+	}
+
+	payload, err := json.Marshal(record)
+	if err != nil {
+		return fmt.Errorf("failed to marshal log record: %w", err)
+	}
+	data := bytes.NewReader(payload)
+
+	resp, err = client.Index(f.Index, data, client.Index.WithContext(ctx))
+	if err != nil {
+		return fmt.Errorf("failed to send ElasticSearch request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.IsError() {
+		return fmt.Errorf("failed to index log record: %s", resp.String())
+	}
+
+	return nil
+}
