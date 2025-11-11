15e3393b06a02d11ef3bf5d9dbf7e9fa0952ac79
Никола Радојевић <nikola@radojevic.rs>
2025-11-11 11:53:59 +0100

    :sparkles: Fix the backend side of the forwarder now that we can test it

--- internal/models/forwarder_v2_clickhouse.go
+++ internal/models/forwarder_v2_clickhouse.go
@@ -2,32 +2,32 @@ package models
 
 import (
 	"context"
-	"crypto/tls"
 	"fmt"
 
 	"github.com/ClickHouse/clickhouse-go/v2"
+	"github.com/google/uuid"
 )
 
 type ForwarderClickhouseV2 struct {
 	Type     string `json:"type" enum:"clickhouse" required:"true"`
 	Url      string `json:"url" required:"true"`
 	Database string `json:"db" required:"true"`
-	Table    string `json:"table" required:"true"`
+	Table    string `json:"table" required:"true" pattern:"^[a-zA-Z_][a-zA-Z0-9_]*$" minLength:"1" maxLength:"64"`
 	Username string `json:"user" required:"true"`
 	Password string `json:"pass" required:"true"`
 }
 
 var createDbQuery = `
-CREATE TABLE IF NOT EXISTS ? (
-	id         uuid                 NOT NULL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS %s (
+	id         UUID                 NOT NULL PRIMARY KEY,
 	timestamp  DateTime64(3, 'UTC') NOT NULL,
 	fields     Map(String, String)  NOT NULL,
 ) ENGINE = MergeTree
 `
 
 var insertLogQuery = `
-INSERT INTO ?
-VALUES (toUUID(rand64()), ?, ?)
+INSERT INTO %s
+VALUES (?, ?, ?)
 `
 
 func (f *ForwarderClickhouseV2) call(ctx context.Context, record *LogRecord) error {
@@ -46,9 +46,7 @@ func (f *ForwarderClickhouseV2) call(ctx context.Context, record *LogRecord) err
 				{Name: "TODO", Version: "TODO"},
 			},
 		},
-		TLS: &tls.Config{
-			InsecureSkipVerify: true, // TODO
-		},
+		TLS: nil,
 	})
 	if err != nil {
 		return fmt.Errorf("failed to initialize connection: %w", err)
@@ -59,12 +57,18 @@ func (f *ForwarderClickhouseV2) call(ctx context.Context, record *LogRecord) err
 		return fmt.Errorf("failed to ping server: %w", err)
 	}
 
-	if err := conn.Exec(ctx, createDbQuery, f.Table); err != nil {
+	query := fmt.Sprintf(createDbQuery, f.Table)
+	if err := conn.Exec(ctx, query); err != nil {
 		return fmt.Errorf("failed to create table: %w", err)
 	}
 
-	if err := conn.Exec(ctx, insertLogQuery, f.Table,
-		record.Timestamp, record); err != nil {
+	pk, err := uuid.NewRandom()
+	if err != nil {
+		return fmt.Errorf("failed to generate uuid: %w", err)
+	}
+
+	query = fmt.Sprintf(insertLogQuery, f.Table)
+	if err := conn.Exec(ctx, query, pk, record.Timestamp, record.Fields); err != nil {
 		return fmt.Errorf("failed to insert row: %w", err)
 	}
 
