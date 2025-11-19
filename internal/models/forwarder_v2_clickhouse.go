2000315e10f3993808b5c08b3749084a73c62e04
David Delassus <david.jose.delassus@gmail.com>
2025-11-19 22:27:44 +0100

    :card_file_box: add validators on Clickhouse forwarder model

--- internal/models/forwarder_v2_clickhouse.go
+++ internal/models/forwarder_v2_clickhouse.go
@@ -18,11 +18,11 @@ type forwarderStateClickhouseV2 struct {
 
 type ForwarderClickhouseV2 struct {
 	Type     string `json:"type" enum:"clickhouse" required:"true"`
-	Address  string `json:"address" required:"true"`
-	Database string `json:"db" required:"true"`
+	Address  string `json:"address" required:"true" pattern:"^(([a-zA-Z0-9.-]+)|(\\[[0-9A-Fa-f:]+\\])):[0-9]{1,5}$"`
+	Database string `json:"db" required:"true" minLength:"1"`
 	Table    string `json:"table" required:"true" pattern:"^[a-zA-Z_][a-zA-Z0-9_]*$" minLength:"1" maxLength:"64"`
-	Username string `json:"user" required:"true"`
-	Password string `json:"pass" required:"true"`
+	Username string `json:"user" required:"true" minLength:"1"`
+	Password string `json:"pass" required:"true" minLength:"1"`
 	UseTls   bool   `json:"tls" required:"true"`
 
 	state *forwarderStateClickhouseV2
