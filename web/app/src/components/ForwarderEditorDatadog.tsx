298172654dabff65f7a9296c10d7c5033e5a5f16
glooopsies <>
2026-01-13 15:22:59 +0100

    :sparkles: Dynamic Fields for Datadog forwarder

--- web/app/src/components/ForwarderEditorDatadog.tsx
+++ web/app/src/components/ForwarderEditorDatadog.tsx
@@ -5,9 +5,12 @@ import TextField from '@mui/material/TextField'
 
 import { useInput } from '@/lib/hooks/input'
 
+import { DynamicField } from '@/lib/models/DynamicField.ts'
 import ForwarderConfigDatadogModel from '@/lib/models/ForwarderConfigDatadogModel'
 
 import * as validators from '@/lib/validators'
+
+import DynamicFieldControl from '@/components/DynamicFieldControl.tsx'
 
 type ForwarderEditorDatadogProps = {
   config: ForwarderConfigDatadogModel
@@ -26,6 +29,28 @@ const ForwarderEditorDatadog = ({
   ])
   const [apiKey, setApiKey] = useInput(config.apiKey, [validators.minLength(1)])
 
+  const [ddsource, setDdsource] = useInput<DynamicField<string>>(
+    config.ddsource,
+    [validators.dynamicField([])]
+  )
+
+  const [ddtags, setDdtags] = useInput<DynamicField<string>>(config.ddtags, [
+    validators.dynamicField([]),
+  ])
+
+  const [hostname, setHostname] = useInput<DynamicField<string>>(
+    config.hostname,
+    [validators.dynamicField([])]
+  )
+
+  const [message, setMessage] = useInput<DynamicField<string>>(config.message, [
+    validators.dynamicField([]),
+  ])
+
+  const [service, setService] = useInput<DynamicField<string>>(config.service, [
+    validators.dynamicField([]),
+  ])
+
   useEffect(() => {
     const valid = url.valid && apiKey.valid
     onValidationChange(valid)
@@ -35,6 +60,11 @@ const ForwarderEditorDatadog = ({
         type: 'datadog',
         url: url.value,
         apiKey: apiKey.value,
+        ddsource: ddsource.value,
+        ddtags: ddtags.value,
+        hostname: hostname.value,
+        message: message.value,
+        service: service.value,
       })
     }
   }, [url, apiKey])
@@ -68,6 +98,47 @@ const ForwarderEditorDatadog = ({
         onChange={(e) => {
           setApiKey(e.target.value)
         }}
+      />
+
+      <DynamicFieldControl
+        id="input:editor.forwarders.datadog.source"
+        label="Source"
+        variant="outlined"
+        error={!ddsource.valid}
+        value={ddsource.value}
+        onChange={setDdsource}
+      />
+      <DynamicFieldControl
+        id="input:editor.forwarders.datadog.tags"
+        label="Tags"
+        variant="outlined"
+        error={!ddtags.valid}
+        value={ddtags.value}
+        onChange={setDdtags}
+      />
+      <DynamicFieldControl
+        id="input:editor.forwarders.datadog.hostname"
+        label="Hostname"
+        variant="outlined"
+        error={!hostname.valid}
+        value={hostname.value}
+        onChange={setHostname}
+      />
+      <DynamicFieldControl
+        id="input:editor.forwarders.datadog.message"
+        label="Message"
+        variant="outlined"
+        error={!message.valid}
+        value={message.value}
+        onChange={setMessage}
+      />
+      <DynamicFieldControl
+        id="input:editor.forwarders.datadog.service"
+        label="Service"
+        variant="outlined"
+        error={!service.valid}
+        value={service.value}
+        onChange={setService}
       />
     </div>
   )
--- web/app/src/lib/models/ForwarderConfigDatadogModel.ts
+++ web/app/src/lib/models/ForwarderConfigDatadogModel.ts
@@ -1,7 +1,14 @@
+import { DynamicField } from '@/lib/models/DynamicField'
+
 type ForwarderConfigDatadogModel = {
   type: 'datadog'
   url: string
   apiKey: string
+  ddsource: DynamicField<string>
+  ddtags: DynamicField<string>
+  hostname: DynamicField<string>
+  message: DynamicField<string>
+  service: DynamicField<string>
 }
 
 export default ForwarderConfigDatadogModel
@@ -10,4 +17,9 @@ export const factory = (): ForwarderConfigDatadogModel => ({
   type: 'datadog',
   url: 'https://http-intake.logs.datadoghq.com/api/v2/logs',
   apiKey: '',
+  ddsource: '@expr:log.ddsource',
+  ddtags: '@expr:log.ddtags',
+  hostname: '@expr:log.hostname',
+  message: '@expr:log.message',
+  service: '@expr:log.service',
 })
