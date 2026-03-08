afb9248a16304fd21c09e7eaaab3ce0887439065
David Delassus <david.jose.delassus@gmail.com>
2026-03-08 11:55:01 +0000

    :sparkles: reintroduce storage synchronization on TCP push/pull

--- internal/storage/cluster-state/main.go
+++ internal/storage/cluster-state/main.go
@@ -1,12 +1,21 @@
 package clusterstate
 
 import (
+	"context"
+
+	"encoding/binary"
+	"fmt"
+
+	"github.com/dgraph-io/badger/v4"
 	"go.uber.org/fx"
 
 	"link-society.com/flowg/internal/utils/kvstore"
 )
 
-type Storage interface{}
+type Storage interface {
+	FetchLocalState(ctx context.Context, nodeID string, endpoints []string) (*NodeState, error)
+	UpdateLocalState(ctx context.Context, nodeID string, namespace string, since uint64) error
+}
 
 type Options struct {
 	Directory string
@@ -36,10 +45,57 @@ func NewStorage(opts Options) fx.Option {
 	kvOpts.Directory = opts.Directory
 
 	return fx.Module(
-		"storage.cluster.state",
+		"cluster.state",
 		kvstore.NewStorage(kvOpts),
 		fx.Provide(func(d deps) Storage {
 			return &storageImpl{kvStore: d.S}
 		}),
 	)
+}
+
+func (s *storageImpl) FetchLocalState(ctx context.Context, nodeID string, endpoints []string) (*NodeState, error) {
+	state := &NodeState{
+		NodeID:   nodeID,
+		LastSync: make(map[string][]NamespaceSyncState),
+	}
+
+	err := s.kvStore.View(ctx, func(txn *badger.Txn) error {
+		prefix := fmt.Appendf(nil, "lastsync:%s:", nodeID)
+		it := txn.NewIterator(badger.DefaultIteratorOptions)
+		defer it.Close()
+
+		for it.Seek(prefix); it.ValidForPrefix(prefix); it.Next() {
+			item := it.Item()
+			key := item.Key()
+			namespace := string(key[len(prefix):])
+
+			var since uint64
+			err := item.Value(func(val []byte) error {
+				since = binary.BigEndian.Uint64(val)
+				return nil
+			})
+			if err != nil {
+				return err
+			}
+
+			state.LastSync[namespace] = append(state.LastSync[namespace], NamespaceSyncState{
+				Namespace: namespace,
+				Since:     since,
+			})
+		}
+
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return state, nil
+}
+
+func (s *storageImpl) UpdateLocalState(ctx context.Context, nodeID string, namespace string, since uint64) error {
+	return s.kvStore.Update(ctx, func(txn *badger.Txn) error {
+		key := fmt.Appendf(nil, "lastsync:%s:%s", nodeID, namespace)
+		return txn.Set(key, binary.BigEndian.AppendUint64(nil, since))
+	})
 }
