b1ee47e1d6978e9ce57c039d2ec2c4c9e6639c3c
Yannick Khalfouni <khalfouni.yannick@gmail.com>
2026-06-17 23:50:53 +0200

    🐛 Preserve scroll position in log viewer watch mode

--- web/app/src/components/LogTable/component.tsx
+++ web/app/src/components/LogTable/component.tsx
@@ -1,20 +1,44 @@
-import { useState } from 'react'
+import { forwardRef, useImperativeHandle, useRef, useState } from 'react'
 
 import { AgGridReact } from 'ag-grid-react'
 
 import LogEntryModel from '@/lib/models/LogEntryModel'
 
 import { LogTableContainer, LogTableDetailPre, LogTableDrawer } from './styles'
-import { LogTableProps } from './types'
+import { LogTableHandle, LogTableProps } from './types'
 
-const LogTable = (props: LogTableProps) => {
+const LogTable = forwardRef<LogTableHandle, LogTableProps>((props, ref) => {
+  const gridRef = useRef<AgGridReact<LogEntryModel>>(null)
   const [selectedRow, setSelectedRow] = useState<LogEntryModel | undefined>(
     undefined
   )
 
+  useImperativeHandle(ref, () => ({
+    appendRows: (rows) => {
+      const api = gridRef.current?.api
+      if (!api) {
+        return
+      }
+
+      const scrollTop = api.getVerticalPixelRange().top
+      const atTop = scrollTop <= 1
+
+      api.applyTransaction({ add: rows })
+
+      if (!atTop) {
+        const rowHeight = api.getDisplayedRowAtIndex(0)?.rowHeight ?? 0
+        if (rowHeight > 0) {
+          const firstVisibleIndex = Math.round(scrollTop / rowHeight)
+          api.ensureIndexVisible(firstVisibleIndex + rows.length, 'top')
+        }
+      }
+    },
+  }))
+
   return (
     <LogTableContainer className="ag-theme-balham">
       <AgGridReact<LogEntryModel>
+        ref={gridRef}
         rowData={props.rowData}
         columnDefs={props.columnDefs}
         suppressFieldDotNotation
@@ -35,6 +59,6 @@ const LogTable = (props: LogTableProps) => {
       </LogTableDrawer>
     </LogTableContainer>
   )
-}
+})
 
 export default LogTable
