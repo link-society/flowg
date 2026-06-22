ff29a3e209f2e74f3df5aa4948426fa4ff1cd13e
David Delassus <david.jose.delassus@gmail.com>
2026-06-22 19:35:39 +0200

    :fire: :white_check_mark: remove clustering test suites

--- tests/config/dns/dns-management-server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -eu
-
-PORT="${PORT:-8080}"
-
-socat TCP-LISTEN:"$PORT",reuseaddr,fork SYSTEM:"/dns-management-router.sh"
--- tests/src/web/test_e2e.py
+++ tests/src/web/test_e2e.py
@@ -5,7 +5,7 @@ from ..settings import has_test_suite
 
 
 @pytest.mark.skipif(not has_test_suite("web"), reason="Web test suite excluded")
-def test_robot(flowg_cluster, spec_dir, report_dir):
+def test_robot(flowg_server, spec_dir, report_dir):
     print("Running Robot Framework:\n")
 
     rc = robot.run(spec_dir, outputdir=report_dir)
