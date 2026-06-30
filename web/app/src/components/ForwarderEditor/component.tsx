afb5030ccde9e9cda8ffb7bfe3b2f2d5c5ea02e1
glooopsies <petar@radojevic.rs>
2026-06-30 15:46:14 +0200

    :lipstick: Implement UI for Google Cloud Log forwarder

--- web/app/src/components/ForwarderEditor/component.tsx
+++ web/app/src/components/ForwarderEditor/component.tsx
@@ -12,6 +12,7 @@ import ForwarderEditorClickhouse from '@/components/ForwarderEditorClickhouse/co
 import ForwarderEditorCloudWatch from '@/components/ForwarderEditorCloudWatch/component.tsx'
 import ForwarderEditorDatadog from '@/components/ForwarderEditorDatadog/component'
 import ForwarderEditorElastic from '@/components/ForwarderEditorElastic/component'
+import ForwarderEditorGoogleCloudLogging from '@/components/ForwarderEditorGoogleCloudLogging/component'
 import ForwarderEditorHttp from '@/components/ForwarderEditorHttp/component'
 import ForwarderEditorOtlp from '@/components/ForwarderEditorOtlp/component'
 import ForwarderEditorSplunk from '@/components/ForwarderEditorSplunk/component'
@@ -30,6 +31,7 @@ const editors = {
   splunk: ForwarderEditorSplunk,
   syslog: ForwarderEditorSyslog,
   cloudwatch: ForwarderEditorCloudWatch,
+  googlecloudlogging: ForwarderEditorGoogleCloudLogging,
 }
 
 const ForwarderEditor = ({
---
+++ web/app/src/lib/models/ForwarderConfigGoogleCloudLoggingModel.ts
@@ -0,0 +1,15 @@
+type ForwarderConfigGoogleCloudLoggingModel = {
+  type: 'googlecloudlogging'
+  project_id: string
+  log_id: string
+  endpoint: string
+}
+
+export default ForwarderConfigGoogleCloudLoggingModel
+
+export const factory = (): ForwarderConfigGoogleCloudLoggingModel => ({
+  type: 'googlecloudlogging',
+  project_id: 'flowg',
+  log_id: 'flowg',
+  endpoint: 'logging.googleapis.com',
+})
