19bc28747dd4bdb07bd0314078b26477661fa338
devnoct <peppinoilbukkino@gmail.com>
2026-07-01 12:29:43 +0000

    ✨ Add AuthProvider doc comments

--- internal/models/auth_provider_saml.go
+++ internal/models/auth_provider_saml.go
@@ -1,6 +1,8 @@
 package models
 
-type SamlAuthProvider struct {
+// AuthProviderSaml contains the configuration for a SAML authentication
+// provider.
+type AuthProviderSaml struct {
 	Type           string `json:"type" enum:"saml" required:"true"`
 	IdpMetadataURL string `json:"idp_metadata_url" required:"true" format:"uri"`
 }
