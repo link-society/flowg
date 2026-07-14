19408c056ea1b9fe82b8c935c7afbdcbe25e7bca
David Delassus <david.jose.delassus@gmail.com>
2026-07-14 17:27:40 +0200

    :coffin: dynamic field compilation was moved to the `internal/engines/forwarders` package

--- internal/models/dynamic_field.go
+++ internal/models/dynamic_field.go
@@ -1,13 +1,6 @@
 package models
 
-import (
-	"strconv"
-
-	"github.com/expr-lang/expr"
-	"github.com/expr-lang/expr/vm"
-
-	"github.com/swaggest/jsonschema-go"
-)
+import "github.com/swaggest/jsonschema-go"
 
 // DynamicField is a forwarder configuration value that may either be a literal
 // string or an [expr](https://expr-lang.org/) expression (when prefixed with
@@ -20,23 +13,4 @@ func (DynamicField) PrepareJSONSchema(schema *jsonschema.Schema) error {
 	schema.AddType(jsonschema.String)
 	schema.WithPattern(`^@expr:`)
 	return nil
-}
-
-// CompileDynamicField compiles a dynamic field into an expr program. A value
-// prefixed with "@expr:" is compiled as an expression; any other value is
-// compiled as a quoted string literal, so plain values evaluate to themselves.
-func CompileDynamicField(value string) (*vm.Program, error) {
-	if len(value) >= 6 && value[:6] == "@expr:" {
-		return expr.Compile(
-			value[6:],
-			expr.Env(map[string]any{}),
-			expr.AllowUndefinedVariables(),
-		)
-	} else {
-		return expr.Compile(
-			strconv.Quote(value),
-			expr.Env(map[string]any{}),
-			expr.AllowUndefinedVariables(),
-		)
-	}
 }
