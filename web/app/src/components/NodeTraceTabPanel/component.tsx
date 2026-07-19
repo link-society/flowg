1c398c998633635085f8238550e3aeca74c919bc
Yannick Khalfouni <khalfouni.yannick@gmail.com>
2026-07-20 00:25:40 +0200

    🌐 Translate remaining hardcoded strings in forwarder types and log chart

--- web/app/src/components/NodeTraceTabPanel/component.tsx
+++ web/app/src/components/NodeTraceTabPanel/component.tsx
@@ -1,36 +1,54 @@
+import { useTranslation } from 'react-i18next'
+
 import { TraceCode, TraceLabel, TraceRow, TraceSection } from './styles'
 import { NodeTraceTabPanelProps } from './types'
 
-const NodeTraceTabPanel = ({ trace, index, value }: NodeTraceTabPanelProps) => (
-  <div role="tabpanel" hidden={value !== index} key={index}>
-    {trace.error && (
-      <TraceSection>
-        <TraceLabel>Error:</TraceLabel>
-        <TraceCode id="container:transformers.test.result" variant="outlined">
-          {trace.error}
-        </TraceCode>
-      </TraceSection>
-    )}
-    <TraceRow>
-      {trace.input && (
-        <TraceSection>
-          <TraceLabel variant="text">Input Record:</TraceLabel>
-          <TraceCode id="container:transformers.test.result" variant="outlined">
-            {JSON.stringify(trace.input, null, 2)}
-          </TraceCode>
-        </TraceSection>
-      )}
+const NodeTraceTabPanel = ({ trace, index, value }: NodeTraceTabPanelProps) => {
+  const { t } = useTranslation()
 
-      {trace.output && (
+  return (
+    <div role="tabpanel" hidden={value !== index} key={index}>
+      {trace.error && (
         <TraceSection>
-          <TraceLabel variant="text">Output Record(s):</TraceLabel>
+          <TraceLabel>
+            {t('components.nodeTraceTabPanel.errorLabel')}
+          </TraceLabel>
           <TraceCode id="container:transformers.test.result" variant="outlined">
-            {JSON.stringify(trace.output, null, 2)}
+            {trace.error}
           </TraceCode>
         </TraceSection>
       )}
-    </TraceRow>
-  </div>
-)
+      <TraceRow>
+        {trace.input && (
+          <TraceSection>
+            <TraceLabel variant="text">
+              {t('components.nodeTraceTabPanel.inputLabel')}
+            </TraceLabel>
+            <TraceCode
+              id="container:transformers.test.result"
+              variant="outlined"
+            >
+              {JSON.stringify(trace.input, null, 2)}
+            </TraceCode>
+          </TraceSection>
+        )}
+
+        {trace.output && (
+          <TraceSection>
+            <TraceLabel variant="text">
+              {t('components.nodeTraceTabPanel.outputLabel')}
+            </TraceLabel>
+            <TraceCode
+              id="container:transformers.test.result"
+              variant="outlined"
+            >
+              {JSON.stringify(trace.output, null, 2)}
+            </TraceCode>
+          </TraceSection>
+        )}
+      </TraceRow>
+    </div>
+  )
+}
 
 export default NodeTraceTabPanel
