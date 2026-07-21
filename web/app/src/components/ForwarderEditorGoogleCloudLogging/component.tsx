96173db1f979c153ed4ced7095e32e3469169fbc
glooopsies <petar@radojevic.rs>
2026-07-21 15:17:59 +0200

    :card_file_box: Add DisableTLS and DisableAuthentication fields to Google Cloud Logging fowarder

--- web/app/src/components/ForwarderEditorGoogleCloudLogging/component.tsx
+++ web/app/src/components/ForwarderEditorGoogleCloudLogging/component.tsx
@@ -3,6 +3,9 @@ import { useColorMode } from '@/theme'
 import { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
+import Checkbox from '@mui/material/Checkbox'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import FormGroup from '@mui/material/FormGroup'
 import TextField from '@mui/material/TextField'
 
 import Editor, { useMonaco } from '@monaco-editor/react'
@@ -14,7 +17,7 @@ import {
   vrlThemeDefinition,
 } from '@/lib/vrl-highlighter.ts'
 
-import { ForwarderEditorGoogleCloudLoggingRoot } from './styles'
+import {ForwarderEditorGoogleCloudLoggingRoot} from './styles'
 import { ForwarderEditorGoogleCloudLoggingProps } from './types'
 
 const ForwarderEditorGoogleCloudLogging = ({
@@ -26,6 +29,8 @@ const ForwarderEditorGoogleCloudLogging = ({
   const [endpoint, setEndpoint] = useInput(config.endpoint)
   const [project_id, setProjectID] = useInput(config.project_id)
   const [log_id, setLogID] = useInput(config.log_id)
+  const [disable_tls, setDisableTls] = useInput(config.disable_tls)
+  const [disable_auth, setDisableAuth] = useInput(config.disable_auth)
   const [auth_json, setAuthJson] = useState(config.auth_json)
 
   const monaco = useMonaco()
@@ -52,10 +57,19 @@ const ForwarderEditorGoogleCloudLogging = ({
         endpoint: endpoint.value,
         project_id: project_id.value,
         log_id: log_id.value,
+        disable_auth: disable_auth.value,
+        disable_tls: disable_tls.value,
         auth_json,
       })
     }
-  }, [endpoint, project_id, log_id, auth_json])
+  }, [
+    endpoint,
+    project_id,
+    log_id,
+    disable_auth,
+    disable_tls,
+    auth_json,
+  ])
 
   return (
     <ForwarderEditorGoogleCloudLoggingRoot id="container:editor.forwarders.googlelog">
@@ -95,17 +109,53 @@ const ForwarderEditorGoogleCloudLogging = ({
         }}
       />
 
-      <label>
-        {t('components.forwarderEditorGoogleCloudLogging.authJsonLabel')}
-      </label>
-      <Editor
-        defaultValue={auth_json}
-        defaultLanguage="json"
-        height="10rem"
-        theme={mode === 'dark' ? 'vrl-theme-dark' : 'vrl-theme-light'}
-        onChange={setAuthJson}
-        options={{ minimap: { enabled: false } }}
-      />
+      <FormGroup>
+        <FormControlLabel
+          control={
+            <Checkbox
+              id="input:editor.forwarders.googlelog.disable_tls"
+              checked={disable_tls.value}
+              onChange={(e) => {
+                setDisableTls(e.target.checked)
+              }}
+            />
+          }
+          label={t(
+            'components.forwarderEditorGoogleCloudLogging.allowInsecureLabel'
+          )}
+        />
+
+        <FormControlLabel
+          control={
+            <Checkbox
+              id="input:editor.forwarders.googlelog.disable_auth"
+              checked={disable_auth.value}
+              onChange={(e) => {
+                setDisableAuth(e.target.checked)
+              }}
+            />
+          }
+          label={t(
+            'components.forwarderEditorGoogleCloudLogging.disableAuthLabel'
+          )}
+        />
+      </FormGroup>
+
+      {!disable_auth.value && (
+        <>
+          <label>
+            {t('components.forwarderEditorGoogleCloudLogging.authJsonLabel')}
+          </label>
+          <Editor
+            defaultValue={auth_json}
+            defaultLanguage="json"
+            height="10rem"
+            theme={mode === 'dark' ? 'vrl-theme-dark' : 'vrl-theme-light'}
+            onChange={setAuthJson}
+            options={{ minimap: { enabled: false } }}
+          />
+        </>
+      )}
     </ForwarderEditorGoogleCloudLoggingRoot>
   )
 }
